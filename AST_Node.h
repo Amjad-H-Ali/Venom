0c6d877c37dfa85427875591e8e9ce5aea9aadd1
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-05 02:12:07 -0500

    Defined AST_NODE_LIST

---
+++ AST_Node.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// AST_NODE_LIST takes in a Macro(N).
+// Each Macro has a signature(type),
+// where type is a unique symbol of 
+// an AST Node.
+
+// N: Unique Symbol of each Node in AST
+#define AST_NODE_LIST(N) 			\
+	N(AST_ID)						\
+	N(AST_IS)						\
+	N(AST_STRING)					\
+	N(AST_LIST)
+
+// All AST_Node Symbols
+#define N(symbol),
+	enum AST_SYMBOLS {AST_NODE_LIST(N)};
+#undef N
+
+
+class AST_Node {
+
+	token::Symbol type;
+
+
+
+};
