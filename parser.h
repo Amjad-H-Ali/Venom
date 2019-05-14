57beb55936377f0ac26dfd910e22c40b84fa54c0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-05-14 02:56:39 -0500

    Declared functions

--- parser.h
+++ parser.h
@@ -10,28 +10,39 @@ namespace parser {
 
 namespace utility {
 
-	bool validStartToList(token::Token *tokenPtr);
+	bool validStartToListOrParams(token::Token *tokenPtr);
+
+	bool validStartToFunctionBlock(token::Token *tokenPtr);
 
 }; // utility
 
 
 
-// Parser functions
+	// Parser functions
 
-// Main parser
-AST_Node *parser(token::Token *current);
+	// Main parser
+	AST_Node *wrapperParser(token::Token *current);
 
+	AST_Node *parser(token::Token *tokenPtr) ;
 
-// Takes in a Token from a Linked List and Parses it.
-// Returns an AST Node.
-AST_Node *parseToken(token::Token *current);
+	// Takes in a Token from a Linked List and Parses it.
+	// Returns an AST Node.
+	AST_Node *parseToken(token::Token *current);
 
-// Parse operands of an AST operator node
-AST_Node *parseOperand(token::Token *tokenPtr);
+	// Parse operands of an AST operator node
+	AST_Node *parseOperand(token::Token *tokenPtr);
 
+	// Parse List of an AST list node
+	AST_Node *parseListOrParams(token::Token *tokenPtr, bool &isParams, token::Token *&startToBlock);
 
-// Parse List of an AST list node
-AST_Node *parseList(token::Token *tokenPtr);
+	// Parse List
+	AST_Node *parseList();
+
+	// Parse parameters
+	AST_Node *parseParams(token::Token *tokenPtr);
+
+	// Parse statement block
+	AST_Node *parseBlock(token::Token *tokenPtr);
 
 
 }; // parser
