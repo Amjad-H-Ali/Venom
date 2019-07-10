67927b711dfe611c1811dc83ee055e15c6c848c1
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-07-09 23:14:36 -0500

    Modified data member in AST class to work with changes made to variant

--- AST.h
+++ AST.h
@@ -15,23 +15,25 @@ class AST_Block;
 class AST_BinOp;
 
 /*
- 	To create Linked-List of objects of different types. Why not use their parent since they all share the same
- 	one? The reason: Due to some objects having different member variables than their siblings, overloaded 
- 	set_value/get_value methods were needed. I did not want to add all the various methods as virtual methods 
- 	in the Base class. Using std::variant allows me to call   
+ 	* To create Linked-List of objects of different types. Why not use their parent since they all share the same
+ 	* one? The reason is due to some objects having different member variables than their siblings, overloaded 
+ 	* set_value/get_value methods were needed. I did not want to add all the various methods as virtual methods 
+ 	* in the Base class.   
 */
 typedef std::variant< std::nullptr_t, AST_Str , AST_List , AST_ID , AST_Func , AST_Block , AST_BinOp > ast_t;
 
-// See AST.cpp
+/*
+	* See AST.cpp
+*/
 extern bool operator!(const ast_t &astObj);
 
 /*
-	AST_LIST takes in a Macro(N).
-	Each Macro has a signature(type),
-	where type is a unique symbol of 
-	an AST_Node.
+	* AST_LIST takes in a Macro(N).
+	* Each Macro has a signature(type),
+	* where type is a unique symbol of 
+	* an AST_Node.
 
-	N: Unique Symbol of each Node in AST
+	* N: Unique Symbol of each Node in AST
 */
 
 namespace ast {
@@ -56,7 +58,7 @@ namespace ast {
 
 struct AST {
 
-	astPtr_t node;
+	ast_t *node;
 
 	AST *next, *prev;
 
@@ -67,7 +69,9 @@ struct AST {
 
 	AST *getValueOfNode();
 
-	// Overload operator.
+	/*
+		* Overload operator.
+	*/
 	bool operator==(ast::AST_SYMBOL type);
 };
 
