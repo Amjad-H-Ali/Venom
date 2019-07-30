337e404737adf2da2041b17a8f2a35855e2a2cec
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-07-30 01:22:02 -0500

    Defined VM

---
+++ VM/VM.h
@@ -0,0 +1,45 @@
+#ifndef VM_H
+
+#define VM_H
+
+class VM {
+
+private:
+
+	/*
+	 +++++++ Activation Records (Stack Frames) of called funtions are stored here +++++++++++
+	 */
+
+	Stack<> *callStack;
+
+	/*
+	 ++++++ ByteCode to execute. ++++++++
+	 */
+
+	Queue<> *execQ;
+
+	/*
+	 ++++++++ Used to map variable names to their values in O(1) time complexity ++++++++++++
+	 */
+
+	Trie<> *declTree;
+
+
+	/*
+	 +++++ Registers to store information such as Function's return address, current stack frame, etc... +++++++++
+	 */
+
+	struct Register;
+
+
+public:
+
+	/*
+	 ++++++ Executes the ByteCode in execQ +++++++++
+	*/
+	void operator()();
+
+}; // VM
+
+
+#endif
