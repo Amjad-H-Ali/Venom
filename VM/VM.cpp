531b6c4e01e2dabcc2d63031b93843443218cebf
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-07-30 01:33:57 -0500

    Defined Register class in VM

---
+++ VM/VM.cpp
@@ -0,0 +1,51 @@
+
+/*
+ +++++ Registers to store information such as Function's return address, current stack frame, etc... +++++++++
+ */
+
+struct VM::Register {
+
+	/*
+     ++++++ Stack Pointer ++++++++
+	 */
+
+	Node<> *esp;
+
+	/*
+     ++++++ Base Pointer +++++++++
+	 */
+
+	Node<> *ebp;
+
+	/*
+     ++++ Instruction Pointer ++++
+	 */
+
+	Node<> *eip;
+
+	/*
+	 ++++ Accumulator Register ++++
+	 */
+
+	Node<> *eax;
+
+
+
+	/*
+	 +++++ Main C'tor +++++++
+	 */
+
+	Register()
+	
+		:esp(nullptr), ebp(nullptr), eip(nullptr), eax(nullptr)
+	{};
+
+}; // Register
+
+
+
+
+/*
+ ++++++ Executes the ByteCode in execQ +++++++++
+*/
+void VM::operator()();
