627aae0a14d7277885859849ef01b369947ddaae
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-08-03 02:51:16 -0500

    Changed Pointers to SharedPtr. Also added method to erase data

--- utility/Trie.h
+++ utility/Trie.h
@@ -2,25 +2,39 @@
 
 #define TRIE_H
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                     *
+ * Trie structure that supports strings and ints as    *
+ * keys to create a map to their corresponding data.   *
+ *                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 template<typename T>
 
 class Trie {
 
 private:
 
+
     /*
-     +++++++ All potential single char ASCII(s) that may be used as keys +++++++++++++
+     +++++ All potential single char ASCII(s) that may be used as keys +++++
      */
 
     static constexpr size_t NUM_OF_SINGLE_ASCII = '~' - '!';
 
+
     /*
-     ++++++ Node for creating paths to the symbols ++++++++
+     +++++ Node for creating paths to the data +++++
      */
 
     struct Node;
 
-    Node *head;
+
+    /*
+     +++++ Pointer to the begining of the Trie +++++
+     */
+
+    SharedPtr<Node> head;
 
     /*
      ++++ Returns unique key ++++
@@ -28,50 +42,75 @@ private:
 
     size_t hash(char letter) const;
 
+
+    /*
+     +++++ Returns the destination Node +++++
+     */
+
+    SharedPtr<Node> goTo(size_t indx) const;
+
+
+    /*
+     +++++ Returns the destination Node +++++
+     */
+
+    SharedPtr<Node> goTo(const std::string &data,  std::string::size_type start = 0, std::string::size_type end = 0) const;
+
+
 public:
+   
+
 
     /*
      +++ Main C'tor +++
-    */ 
+     */
 
     Trie();
 
     /*
-     +++++ Stores data at a destination that was mapped using str. +++++
+     +++++ Stores data at the destination mapped with str. +++++
      */
 
-    void push(const std::string &str, T *data = nullptr);
+    void push(const std::string &str, const SharedPtr<T> &data);
 
     /*
      +++++ Stores data at a destination that was mapped using index. +++++
      */
 
-    void push(size_t indx, T *data = nullptr);
+    void push(size_t indx, const SharedPtr<T> &data);
 
     /*
-     +++++++++ Finds corresponding symbol of string in O(1) time complexity.         ++++++++++
-     +++++++++ Params: string containing data and the range (start and end) to read. ++++++++++
-     +++++++++ Range is set to full string as defualt.                               ++++++++++
+     +++++ Finds corresponding data of string in O(1) time complexity. Range of string is allowed ++++++                          
      */
 
-    T *map(const std::string &data,  std::string::size_type start = 0, std::string::size_type end = 0);
+    SharedPtr<T> map(const std::string &data,  std::string::size_type start = 0, std::string::size_type end = 0) const;
 
     /*
      +++++ Uses indx to map to destination and returns the data found there +++++
      */ 
 
-    T *map(size_t indx);
+    SharedPtr<T> map(size_t indx) const;
 
     /*
-     ++++++++++++ Gets the index of the string where mapping broke off.               +++++++++++++++++++
-     ++++++++++++ Params: the string containing the key and the position of where to  +++++++++++++++++++
-     ++++++++++++ start reading the string (set to begining of string as defualt).    +++++++++++++++++++
+     +++++ Goes to destination and sets data to nullptr +++++
      */
 
-    std::string::size_type getBreakPoint(const std::string &data, std::string::size_type start = 0);
+    void eraseData(size_t indx);
 
+    /*
+     +++++ Goes to destination and sets data to nullptr +++++
+     */
+
+    void eraseData(const std::string &str);
+
+
+    /*
+     +++++ Finds the position of input string where mapping broke off. +++++
+     +++++ ie:  Map In Trie -> "HELLO",  Input -> "HEL", Output -> 3   +++++
+     */
+
+    std::string::size_type getBreakPoint(const std::string &data, std::string::size_type start = 0) const;
     
-
 }; // Trie
 
 
