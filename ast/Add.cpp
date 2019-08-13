485ddcac1ea369164fb2704dac7f28cc4928bb0f
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-08-13 13:46:22 -0500

    Implemented Rule of 5 for Add class

--- ast/Add.cpp
+++ ast/Add.cpp
@@ -1,15 +1,87 @@
 #include "Add.h"
 #include "AST.h"
 
+/*
+ +++++ MAIN C'TOR +++++
+ */
+
 Add::Add(ast_t&& lOperandParam, ast_t&& rOperandParam)
 
 	:lOperand(new ast_t(std::move(lOperandParam))), rOperand(new ast_t(std::move(rOperandParam)))
-
 {}
+
+
+/*
+ +++++ RULE OF FIVE +++++
+ */
+
+/* 1
+ +++++ COPY C'TOR +++++
+ */
+
+Add(const Add& addObj) {
+
+	lOperand = new lOperand(addObj.lOperand);
+
+	rOperand = new rOperand(addObj.rOperand);
+}
+
+/* 2
+ +++++ MOVE C'TOR +++++
+ */
+
+Add(Add&& addObj)
+	:lOperand(addObj.lOperand), rOperand(addObj.rOperand);
+{
+
+	addObj.lOperand = nullptr;
+
+	addObj.rOperand = nullptr;
+}
+
+/* 3
+ +++++ COPY ASSIGN +++++
+ */
+
+Add& operator=(const Add& addObj) {
+
+	lOperand = new lOperand(addObj.lOperand);
+
+	rOperand = new rOperand(addObj.rOperand);
+
+	return *this;
+};
+
+/* 4
+ +++++ MOVE ASSIGN +++++
+ */
+
+Add& operator=(Add&& addObj) {
+
+	lOperand = addObj.lOperand;
+
+	rOperand = addObj.rOperand;
+
+	addObj.lOperand = nullptr;
+
+	addObj.rOperand = nullptr;
+
+	return *this;
+
+}
+
+/* 5
+ +++++ DESTRUCTOR +++++
+ */
 
 Add::~Add() {
 
-	delete lOperand;
+	if(lOperand) delete lOperand;
 
-	delete rOperand;
+	if(rOperand) delete rOperand;
+
+	lOperand = nullptr;
+
+	rOperand = nullptr;
+
 }
--- ast/Add.h
+++ ast/Add.h
@@ -18,9 +18,43 @@ private:
 
 public:
 
-
+	/*
+	 +++++ MAIN C'TOR +++++
+	 */
+	
 	Add(ast_t&& lOperandParam, ast_t&& rOperandParam);
 
+	/*
+	 +++++ RULE OF FIVE +++++
+	 */
+
+	/* 1
+	 +++++ COPY C'TOR +++++
+	 */
+
+	Add(const Add& addObj);
+
+	/* 2
+	 +++++ MOVE C'TOR +++++
+	 */
+
+	Add(Add&& addObj);
+
+	/* 3
+	 +++++ COPY ASSIGN +++++
+	 */
+
+	Add& operator=(const Add& addObj);
+
+	/* 4
+	 +++++ MOVE ASSIGN +++++
+	 */
+
+	Add& operator=(Add&& addObj);
+	
+	/* 5
+	 +++++ DESTRUCTOR +++++
+	 */
 	~Add();
 
 
